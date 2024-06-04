4d9d628843227dac78203bba6a8a00244ef62081
geewjh <geewjh@gmail.com>
2024-06-04 09:29:21 +0800

    feat: install cors

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const logger = require("morgan");
+const cors = require("cors");
+
 require("dotenv").config();
 require("./config/database");
 
 const app = express();
 
+// let corsOption = {
+//   origin: ["https://xxx-xxx.vercel.app"],
+// };
+
+// app.use(cors(corsOption));
+
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/*", function (req, res) {
+app.get("/api", (req, res) => {
   res.send("Hello World");
 });
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Express app running on port ${port}`);
 });
