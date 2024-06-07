a40f123197dbabe8e902d490abf6d17fd116b5dd
geewjh <geewjh@gmail.com>
2024-06-07 13:53:07 +0800

    feat: install jsonwebtoken

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,12 +1,19 @@
+const jwt = require("jsonwebtoken");
 const User = require("../../models/user");
 
 async function create(req, res) {
   try {
     const user = await User.create(req.body);
-    res.status(201).json(user);
+    const token = createJWT(user);
+    res.status(201).json(token);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+}
+
+function createJWT(user) {
+  const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: "2d" });
+  return token;
 }
 
 module.exports = {
