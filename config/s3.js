f18b7f6080ecb1cdf80a499d2a7c9a3691544236
geewjh <geewjh@gmail.com>
2024-06-17 11:46:28 +0800

    fix: resolved merge conflict by setting JPEG quality to 100

--- config/s3.js
+++ config/s3.js
@@ -40,11 +40,7 @@ module.exports = function postToS3(req, res, next) {
       for (const file of req.files) {
         const alteredImage = await sharp(file.buffer)
           .resize(250, 250, { fit: sharp.fit.fill })
-<<<<<<< HEAD
-          .toFormat("jpeg", { mozjpeg: true, quality: 75 })
-=======
           .toFormat("jpeg", { mozjpeg: true, quality: 100 })
->>>>>>> 1aa301f (fix: change quality of image)
           .toBuffer();
 
         const fileBaseName = file.originalname.split(".")[0];
