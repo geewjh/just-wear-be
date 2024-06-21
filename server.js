7bc076f53dd5039c387870de3545ec710eeb135f
geewjh <geewjh@gmail.com>
2024-06-21 16:39:41 +0800

    feat: cors

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ require("./config/database");
 
 const app = express();
 
-// let corsOption = {
-//   origin: ["https://xxx-xxx.vercel.app"],
-// };
+let corsOption = {
+  origin: ["https://main.d2hnmd97ss8ky2.amplifyapp.com"],
+};
 
-// app.use(cors(corsOption));
+app.use(cors(corsOption));
 
-app.use(cors());
+// app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
